7f112d38125fe9d49d6a2217266b678304a7fc19
Dan Stefan Oprean <dan@designxenon.com>
2021-09-26 16:31:18 +0300

    Add ability to get the database and wokr with it directly.

--- bitcaskkv.go
+++ bitcaskkv.go
@@ -7,7 +7,7 @@ import (
 	"git.mills.io/prologic/bitcask"
 )
 
-type KVStore struct {
+type Store struct {
 	db *bitcask.Bitcask
 }
 
@@ -16,15 +16,15 @@ var (
 	ErrBadValue = errors.New("bitcaskkv: bad value")
 )
 
-func Open(path string) (*KVStore, error) {
+func Open(path string) (*Store, error) {
 	if db, err := bitcask.Open(path); err != nil {
 		return nil, err
 	} else {
-		return &KVStore{db: db}, nil
+		return &Store{db: db}, nil
 	}
 }
 
-func (s *KVStore) Put(key string, value interface{}) error {
+func (s *Store) Put(key string, value interface{}) error {
 	if value == nil {
 		return ErrBadValue
 	}
@@ -35,12 +35,12 @@ func (s *KVStore) Put(key string, value interface{}) error {
 
 	if err := s.db.Put([]byte(key), buf.Bytes()); err != nil {
 		return err
-	} else {
-		return s.db.Sync()
 	}
+
+	return nil
 }
 
-func (s *KVStore) Get(key string, value interface{}) error {
+func (s *Store) Get(key string, value interface{}) error {
 	v, err := s.db.Get([]byte(key))
 	if err != nil {
 		return ErrNotFound
@@ -52,26 +52,21 @@ func (s *KVStore) Get(key string, value interface{}) error {
 	}
 }
 
-func (s *KVStore) Delete(key string) error {
+func (s *Store) Delete(key string) error {
 	_, err := s.db.Get([]byte(key))
 	if err != nil {
 		return ErrNotFound
 	}
 	if err = s.db.Delete([]byte(key)); err != nil {
 		return err
-	} else {
-		return s.db.Sync()
 	}
+	return nil
 }
 
-func (s *KVStore) Keys() chan []byte {
-	return s.db.Keys()
-}
-
-func (s *KVStore) Scan(prefix []byte, f func(key []byte) error) error {
-	return s.db.Scan(prefix, f)
-}
-
-func (s *KVStore) Close() error {
+func (s *Store) Close() error {
 	return s.db.Close()
+}
+
+func (s *Store) GetDb() *bitcask.Bitcask {
+	return s.db
 }
